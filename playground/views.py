0fee641f53ed2b09a38273d87eb361a962fb77ef
Fabio Girones <fdgirones@gmail.com>
2025-04-09 14:40:55 +0200

    Added various amount of ORM queries

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,42 @@
 from django.shortcuts import render
+from django.db.models import Q, F, Value, Func, Count, Max, Min, Avg, Sum, ExpressionWrapper, DecimalField
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product, Order, Customer
 
 # Create your views here.
 def say_hello(request):
-    return render(request, 'hello.html', { 'name': 'Mosh' })
+    try:
+        # products = Product.objects.filter(unit_price__range=(20, 30))
+        # products2 = Product.objects.filter(inventory__lt=10, unit_price__lt=20) #returns queryset
+        # queryset_qobjects = Product.objects.filter(Q(inventory_lt=10) | ~Q(unit_price__lt=20))
+        # queryset_fobjects = Product.objects.filter(inventory=F('collection_id')) #comparing 2 fields
+        # queryset_orderby = Product.objects.order_by('unit_price', '-title')
+        # queryset_distinct = Product.objects.values('id') #dictionary objects
+        # queryset = Product.objects.only('id', 'title') #instances of the product class
+        # select_related (1) (n-->1)
+        # select_prefetch_related (n) (n-->n)
+        # latest_orders = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+        # result = Product.objects.aggregate(count=Count('id'), min_price=Min('unit_price'))
+        
+        # queryset = Customer.objects.annotate(
+        #     full_name=Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT')
+        # )
+
+        # queryset = Customer.objects.annotate(
+        #     full_name=Concat('first_name', Value(' '), 'last_name')
+        # )
+
+        discounted_price = ExpressionWrapper(
+            F('unit_price') * Value(0.8),
+            output_field=DecimalField()
+        )
+
+        queryset = Product.objects.annotate(
+            discounted_price=discounted_price
+        )
+
+    except ObjectDoesNotExist:
+        pass
+
+    return render(request, 'hello.html', { 'products': list(queryset) })
--- storefront/settings.py
+++ storefront/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'playground',
     'store',
     'tags',
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +52,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
 ]
 
 ROOT_URLCONF = 'storefront.urls'
