9b458ddf6e4d4af6a6970dd3e56556f3bd0a29ed
Fabio Girones <fdgirones@gmail.com>
2025-04-12 15:11:43 +0200

    Repaired and improved storefront version 1

--- tags/models.py
+++ tags/models.py
@@ -2,9 +2,7 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
-from store.models import Product
 
-# Create your models here.
 class TaggedItemManager(models.Manager):
     def get_tags_for(self, obj_type, obj_id):
         content_type = ContentType.objects.get_for_model(obj_type)
@@ -16,11 +14,13 @@ class TaggedItemManager(models.Manager):
                 object_id=obj_id
             )
 
+
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.label
+
 
 class TaggedItem(models.Model):
     objects = TaggedItemManager()
